98e75c0683e8b7ca0fa2da46b1ad440fc65305dc
Leo Neumeyer <leo@s4.io>
2011-11-01 15:31:07 -0700

    This should work.

--- src/main/java/s4app/HelloApp.java
+++ src/main/java/s4app/HelloApp.java
@@ -1,11 +1,14 @@
 package s4app;
 
 import org.apache.s4.core.App;
+import org.ejml.data.D1Matrix64F;
+import org.ejml.data.DenseMatrix64F;
 
 public class HelloApp extends App {
 
     @Override
     protected void start() {
+        D1Matrix64F tmpArray = new DenseMatrix64F(10, 1);
         System.out.println("Hello App! I'm starting...");
     }
 
