74472754311493cc4eee9efef205d9d593873520
Leo Neumeyer <leo@s4.io>
2011-11-02 11:09:56 -0700

    Bug fix.

--- src/main/java/s4app/HelloApp.java
+++ src/main/java/s4app/HelloApp.java
@@ -1,14 +1,16 @@
 package s4app;
 
+import java.util.Random;
+
 import org.apache.s4.core.App;
-import org.ejml.data.D1Matrix64F;
 import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.CommonOps;
+import org.ejml.ops.RandomMatrices;
 
 public class HelloApp extends App {
 
     @Override
     protected void start() {
-        D1Matrix64F tmpArray = new DenseMatrix64F(10, 1);
         System.out.println("Hello App! I'm starting...");
     }
 
@@ -16,6 +18,10 @@ public class HelloApp extends App {
     protected void init() {
         System.out.println("Hello App! I'm initing...");
 
+        Random rand = new Random();
+        DenseMatrix64F A = RandomMatrices.createSymmetric(20, -2, 3, rand);
+        String result = Double.toString(CommonOps.det(A));
+        System.out.println(">>> The result is: " + result);
     }
 
     @Override
